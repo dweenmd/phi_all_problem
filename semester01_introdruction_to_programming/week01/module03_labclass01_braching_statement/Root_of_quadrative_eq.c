e1882e1a719d9fd98ead614fb4a1bf9a0b965342
Dween Mohammad <113244284+dweenmd@users.noreply.github.com>
2022-10-20 19:02:25 +0600

    update

--- semester01_introdruction_to_programming/week01/module03_labclass01_braching_statement/Root_of_quadrative_eq.c
+++ semester01_introdruction_to_programming/week01/module03_labclass01_braching_statement/Root_of_quadrative_eq.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 
 int main(){
-    double a,b,c,d,root1,root2,realpart,imagpart;
+    double a,b,c,d,root1,root2,r,i;
     printf("Enter the value (aX^2+bx+c) of a,b,c: ");
     scanf("%lf %lf %lf", &a,&b,&c);
     d=b*b-4*a*c;
@@ -19,9 +19,9 @@ int main(){
     }
     //when discriminant is less then 0;imaginary part.
  else {
-        realpart = -b / (2 * a);
-        imagpart = sqrt(-d) / (2 * a);
-        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realpart, imagpart, realpart, imagpart);
+        r = -b / (2 * a);
+        i = sqrt(-d) / (2 * a);
+        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", r, i, r, i);
     }
     return 0;
 }
