359688438c324c719982359e50d5c28337961507
Dween Mohammad <113244284+dweenmd@users.noreply.github.com>
2022-10-22 18:45:43 +0600

    update

---
+++ semester01_introdruction_to_programming/week01/conceptual_problem/problem_1.c
@@ -0,0 +1,24 @@
+
+/*Four players participate in the playoff tournament.
+The tournament is held according to the following scheme -
+the first player will play with the second, and the third player with the fourth,
+then the winners of the pairs will play in the finals of the tournament.
+It is known that in a match between two players, the one whose skill is greater will win.
+The tournament is called fair if the two players with the highest skills meet in the finals.
+Determine whether the given tournament is fair.
+
+Output YES if the tournament is fair, or NO otherwise.
+The skill of the first,second,third and fourth player is between 1-100.
+Note - All the players skills are distinct.
+>> Sample input -
+3 7 9 5
+4 5 6 9
+5 3 8 1
+6 5 3 2
+>> Sample output -
+YES
+NO
+YES
+NO */
+
+
--- semester01_introdruction_to_programming/week01/module03_labclass01_braching_statement/tempCodeRunnerFile.c
+++ semester01_introdruction_to_programming/week01/module03_labclass01_braching_statement/tempCodeRunnerFile.c
@@ -1,27 +1,18 @@
-#include<math.h>
-#include<stdio.h>
+    int a,b,c;
 
-int main(){
-    double a,b,c,d,root1,root2,realpart,imagpart;
-    printf("Enter the value (aX^2+bx+c) of a,b,c: ");
-    scanf("%lf %lf %lf", &a,&b,&c);
-    d=b*b-4*a*c;
-    //when discriminant is greater then 0;roots are real number
-    if (d > 0) {
-        root1 = (-b + sqrt(d)) / (2 * a);
-        root2 = (-b - sqrt(d)) / (2 * a);
-        printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
+    printf("Enter three slids:\n");
+
+    scanf("%d %d %d", &a,&b,&c);
+
+    if((a+b>c)&&(a+c>b)&&(b+c>a))
+    {
+        printf("YES, The triangle is valid");
     }
-    //when discriminant is 0;roots are equal
-    else if (d == 0) {
-        root1 = root2 = -b / (2 * a);
-        printf("root1 = root2 = %.2lf;", root1);
+    else 
+    {
+        printf("SORRY, The triangle is not valid");
+
     }
-    //when discriminant is less then 0;imaginary part.
- else {
-        realpart = -b / (2 * a);
-        imagpart = sqrt(-d) / (2 * a);
-        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realpart, imagpart, realpart, imagpart);
-    }
+
     return 0;
-}
+} 
