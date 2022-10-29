c2e9de1b5bf797079de6f1a21f918228a4d292c3
Dween Mohammad <113244284+dweenmd@users.noreply.github.com>
2022-10-29 18:53:30 +0600

    update

--- semester01_introdruction_to_programming/week02 ( loop )/module05/nm_program.c
+++ semester01_introdruction_to_programming/week02 ( loop )/module05/nm_program.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
-int main(){
-   printf("I\nlove\nTahsin.\nLet me go!\n");
-return 0;
+int main()
+{
+    int n;
+    printf("Enter your chocolate number:");
+    scanf("%d",n);
+    int choc=n;
+    int pack=n;
+    while(n>=4){
+            choc+=(pack/4);
+            pack=(pack/4)+(pack%4);
+
+            }
+            printf("Total Chlocklet %d",choc);
+
+    return 0;
 }
+
