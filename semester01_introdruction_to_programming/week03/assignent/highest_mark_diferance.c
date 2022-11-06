a0652c928a91b0647962411f480793b32972450c
Dween Mohammad <113244284+dweenmd@users.noreply.github.com>
2022-11-07 01:48:35 +0600

    c

--- semester01_introdruction_to_programming/week03/assignent/highest_mark_diferance.c
+++ semester01_introdruction_to_programming/week03/assignent/highest_mark_diferance.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    int arr[10000];
+    int n,max=0;
+    int arr[1000];
     scanf("%d",&n);
     for (int i=0;i<n;i++){
             scanf("%d",&arr[i]);
+    }
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        int dif=max-arr[i];
+        printf("%d ",dif);
+
     }
 
 return 0;
--- semester01_introdruction_to_programming/week03/assignent/missing_number.c
+++ semester01_introdruction_to_programming/week03/assignent/missing_number.c
@@ -1,22 +1,12 @@
-
 #include<stdio.h>
 int main()
 {
-    long n,sum1,sum2=0;
-    long arr[500001];
-    long i;
-    scanf("%ld",&n);
-    sum1=n*(n+1)/2;
-    for (i=0;i<n-1;i++){
-            scanf("%ld",&arr[i]);
-     }
-     for (i=0;i<n-1;i++){
-        sum2=sum2+arr[i];
-     }
-    int y=sum1-sum2;
-    printf("%ld",y);
+    long t,a,b,c, sum,d;
+    scanf("%ld",&t);
 
-
-     return 0;
-}
+    while(t--){
+    scanf("%ld%ld%ld%ld",&sum,&a,&b,&c);
+    d = sum-(a+b+c);
+    printf("%ld\n",d);
+    }}
 
