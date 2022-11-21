b874330858e18806251dd1472ac52f7aed7b46ba
dweenmd <dween.md01@gmail.com>
2022-11-21 14:25:38 +0600

    update_multiplied_matrix

--- semester01_introdruction_to_programming/week04/module 14.5/matrix_muultiplication.c
+++ semester01_introdruction_to_programming/week04/module 14.5/matrix_muultiplication.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int first[10][10],second[10][10],result[10][10],r1,r2,c1,c2,i,j,k;
+    int first[10][10],second[10][10],result[10][10],r1,r2,c1,c2,i,j,k, sum=0;
 
     printf("Enter rows and column for first matrix : ");
     scanf("%d %d", &r1,&c1 );
@@ -9,26 +9,83 @@ int main()
     scanf("%d %d", &r2,&c2 );
 
     while (c1!=r2){
-        printf("            Error!!\n\ncolumn of first matrix and row of second matrix are not equal. ");
+        printf("\n\t\t\tError!!\n\ncolumn of first matrix and row of second matrix are not equal. ");
 
-    printf("Enter rows and column for first matrix : ");
+    printf("\n\nEnter rows and column for first matrix : ");
     scanf("%d %d", &r1,&c1 );
     printf("Enter rows and column for second matrix : ");
     scanf("%d %d", &r2,&c2 );
 
     }
+     printf("\nEnter Elements for first matrix:");
     //taking input for first matrix..
+    for (i=0;i<r1;i++){
+        
+        for (j=0;j<c1;j++){
+            printf("\nfirst matrix [%d, %d]=",i,j);
+            
+            scanf("%d",&first [i][j]);
+        }
+    }
+     //taking input for second matrix..
+    printf("\nEnter Elements for second matrix:");
+    for (i=0;i<r2; i++){
+
+        for (j=0;j<c2;j++){
+            printf("\nsecond matrix [%d, %d]=",i,j);
+            scanf("%d",&second [i][j]);
+        }
+    }
+    // for matrix maltiplication we have to maintain some order 
+// 1)first matrix row multiplay all elements of second matrix individual column and sum them.also puts the value of sum 
+// in second matrix first row individual column..
+// this order must be maintained.....
+// sum=0;
+// sum=sum + first matrix[i][k]*second matrix [k][j]
+// multiplied[i][j]=sum
+
+
+        //multiplying matrix
+    for(i=0;i<r1; i++){                          //1st matrix er row
+         for (j=0;j<c2;j++){                    //2nd matrix er column
+             for (k=0; k<c1; k++){             //1st er prottek row er elements ba column gula
+
+            sum=sum + first [i] [k] * second[k][j];
+           
+            }    
+            result[i][j]= sum;
+            sum = 0;
+        }
+    }
+
+
+    //print first and second matrix..
+    printf ("\n\nFirst matrix::\n");
     for (i=0;i<r1; i++){
-        for (j=0;j,c1;j++){
-            scanf("%d",first [i][j]);
+        for (j=0;j<c1;j++){
+            printf("  %d",first [i][j]);
         }
+        printf("\n");
     }
-        for (i=0;i<r1; i++){
-        for (j=0;j,c1;j++){
-            printf("%d",first [i][j]);
+    printf ("\n\nSecond matrix::\n");
+    for (i=0;i<r2; i++){
+        for (j=0;j<c2;j++){
+            printf("  %d",second[i][j]);
+        
         }
+        printf("\n");
     }
 
 
+//print multiplaied matrix
+printf("\n\n result matrix:\n");
+
+    for (i=0;i<r1; i++){
+        for (j=0;j<c2;j++){
+            printf("  %d",result[i][j]);
+        
+        }
+        printf("\n");
+    }
     return 0;
 }
