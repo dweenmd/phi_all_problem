adfe1f9875e9c84717d4c2d1e39ff05ccbb07a5d
dweenmd <dween.md01@gmail.com>
2022-11-23 12:15:49 +0600

    u

--- semester01_introdruction_to_programming/week04/module12(string)/reversre_string2.c
+++ semester01_introdruction_to_programming/week04/module12(string)/reversre_string2.c
@@ -3,7 +3,7 @@ int main(){
     char s1[30]="dween mohammad";
     char s2[30];
     // now we find out  1st string lenth
-    int i=0;len=0;
+    int i=0,len=0;
     while (s1[i]!='\0')
     {
         i++;
