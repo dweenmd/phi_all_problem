8e3f7230f17d63a8c5a02185016fdb2ec53e349e
dweenmd <dween.md01@gmail.com>
2022-12-31 11:23:06 +0600

    s

---
+++ semester01_introdruction_to_programming/week06/module22/oddEvenBy(void)Function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void find_parity(int x)
+{
+    if (x%2==0)
+        printf("Even \n");
+    else
+        printf("Odd\n");
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    find_parity(n);
+    return 0;
+
+}
