ffaafec639e9f468d17a3e1809a23a1b0fe0220e
dweenmd <dween.md01@gmail.com>
2022-12-31 11:32:37 +0600

    p

--- semester01_introdruction_to_programming/week06/module22/oddEvenBy(int)Function.c
+++ semester01_introdruction_to_programming/week06/module22/oddEvenBy(int)Function.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+int is_even(int x)
+{
+    if (x%2==0)
+        return 1;
+    else
+        return 0;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if (is_even(n))
+    {
+        printf("Even\n");
 
+    }
+    else
+    {
+        printf("Odd\n");
+    }
+}
