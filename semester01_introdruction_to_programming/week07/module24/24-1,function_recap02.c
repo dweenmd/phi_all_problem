c6f1b49df9a47bd2d8afec86c7fcaadb812698e3
dweenmd <dween.md01@gmail.com>
2023-01-15 20:09:47 +0600

    module24-1

--- semester01_introdruction_to_programming/week07/module24/24-1,function_recap02.c
+++ semester01_introdruction_to_programming/week07/module24/24-1,function_recap02.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
+void solve()
+{
+    int a,b;
+    printf("Enter two number::");
+    scanf ("%d %d", &a,&b);
+    printf("sum of two number:: %d\n", a+b);
+}
 int main ()
 {
-
+    int t,i;
+    printf("Enter Test Case::");
+    scanf("%d",&t);
+    for (i=0; i<t; i++)
+    {
+        solve ();
+    }
     return 0;
 }
