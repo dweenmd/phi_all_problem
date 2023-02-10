a61afd0757a027b73f13af72448775307c5f7241
dweenmd <dween.md01@gmail.com>
2023-02-11 03:16:03 +0600

    pp05

--- semester01_introdruction_to_programming/week07/module25.5/pp04.c
+++ semester01_introdruction_to_programming/week07/module25.5/pp04.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 void implement(int n);
 int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     implement(n);
     return 0;
 }
 void implement(int n)
 {
-    if (n<1)
-    return;
-    else{
-        printf("%d ",n);
-        implement(n-1);
+    if (n < 1)
+        return;
+    else
+    {
+        printf("%d ", n);
+        implement(n - 1);
     }
 }
---
+++ semester01_introdruction_to_programming/week07/module25.5/pp05.c
@@ -0,0 +1,20 @@
+//using recursion
+#include<stdio.h>
+int sum (int n);
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",sum(n));
+    return 0;
+}
+int sum (int n)
+{
+    if(n!=0){
+        return n+ sum(n-1);
+    }
+    else{
+        return n;
+    }
+    
+}
