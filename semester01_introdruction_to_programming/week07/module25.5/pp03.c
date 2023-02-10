b139a3f45b06e47611a38ddc404ef61d63900e59
dweenmd <dween.md01@gmail.com>
2023-02-11 02:10:57 +0600

    update pow of 2

--- semester01_introdruction_to_programming/week07/module25.5/pp03.c
+++ semester01_introdruction_to_programming/week07/module25.5/pp03.c
@@ -5,38 +5,42 @@ int cheak_pow_of_two(int x);
 int main()
 {
     char string[100];
-    int i = 0, value,sum=0;
+    int i = 0, value, sum = 0;
     printf("Enter the Sentenc: ");
     gets(string);
     while (string[i] != '\0')
     {
-        string[i]=string[i]-96;
-        sum=sum+string[i];
+        string[i] = string[i] - 96;
+        sum = sum + string[i];
         i++;
-        
     }
     // printf("%d\n",sum);
     // getch();
-    if (cheak_pow_of_two(sum)==1){
+    if (cheak_pow_of_two(sum) == 1)
+    {
         printf("Yes\n");
-        int count=1;
-        for(i=0;i<sum;i++){
-            if (sum==1){
+        int count = 1;
+        for (i = 0; i < sum; i++)
+        {
+            if (sum == 1)
+            {
                 break;
             }
-            sum=sum/2;
+            sum = sum / 2;
             count++;
         }
-        printf("cost=2^%d",count);
+        printf("cost=2^%d", count);
     }
-    else printf("No");
+    else
+        printf("No");
     return 0;
 }
 int cheak_pow_of_two(int n)
 {
     if (n == 0)
         return 0;
-    while (n != 1) {
+    while (n != 1)
+    {
         if (n % 2 != 0)
             return 0;
         n = n / 2;
