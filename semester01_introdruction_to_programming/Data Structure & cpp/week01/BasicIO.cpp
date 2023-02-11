b12ebf28705300e9f1238cc0a0aca22bb8362d00
dweenmd <dween.md01@gmail.com>
2023-02-11 23:19:38 +0600

    cpp 1st

---
+++ semester01_introdruction_to_programming/Data Structure & cpp/week01/BasicIO.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // variable declare
+    int a;
+    // input
+    cin >> a;
+    //output
+    cout << a;
+    return 0;
+} // namespace std;
---
+++ semester01_introdruction_to_programming/week07/module26/leap_year_distrinct_digit_problem05.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int is_leap_year(int y)
+{
+    if ((y%400==0)||(y%4==0)&&(y%100!=0))
+    return 1;
+    else
+    return 0;
+}
+int digit_district(int n){
+
+}
+int main()
+{
+
+    return 0;
+}
