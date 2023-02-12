e41cb792dddf0543e3596767cf8e7a6111f6cdc9
dweenmd <dween.md01@gmail.com>
2023-02-12 23:57:01 +0600

    update

--- semester01_introdruction_to_programming/week07/module26/leap_year_distrinct_digit_problem05.c
+++ semester01_introdruction_to_programming/week07/module26/leap_year_distrinct_digit_problem05.c
@@ -7,10 +7,24 @@ int is_leap_year(int y)
     return 0;
 }
 int digit_district(int n){
-
+    int ld;
+    int count_digit[10]={0,0,0,0,0,0,0,0,0,0,};
+    while (n>0){
+        ld=n%10;
+        count_digit[ld]++;
+        n/=10;
+    }
+    int i;
+    for (i=0; i<10;i++){
+        printf("%d--> %d\n",i,count_digit[i]);
+    }
 }
 int main()
 {
-
-    return 0;
+    int n;
+    printf("Enter a year:");
+    scanf("%d",n);
+    is_leap_year(n);
+    digit_district(n);
+     return 0;
 }
