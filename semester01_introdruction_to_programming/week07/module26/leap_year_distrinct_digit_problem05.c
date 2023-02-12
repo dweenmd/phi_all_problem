20ffc6b69a6f07b9437a8f67a0b8f2878fadc827
dweenmd <dween.md01@gmail.com>
2023-02-13 01:07:51 +0600

    update

--- semester01_introdruction_to_programming/week07/module26/leap_year_distrinct_digit_problem05.c
+++ semester01_introdruction_to_programming/week07/module26/leap_year_distrinct_digit_problem05.c
@@ -1,30 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
 int is_leap_year(int y)
 {
-    if ((y%400==0)||(y%4==0)&&(y%100!=0))
-    return 1;
+    if ((y % 400 == 0) || (y % 4 == 0) && (y % 100 != 0))
+        return 1;
     else
-    return 0;
+        return 0;
 }
-int digit_district(int n){
+int digit_district(int n)
+{
     int ld;
-    int count_digit[10]={0,0,0,0,0,0,0,0,0,0,};
-    while (n>0){
-        ld=n%10;
+    int count_digit[10] = {0,0,0,0,0,0,0,0,0,0};
+    while (n > 0)
+    {
+        ld = n % 10;
         count_digit[ld]++;
-        n/=10;
+        n /= 10;
     }
+    //    int i;
+    //    for (i=0; i<10;i++){
+    //        printf("%d--> %d\n",i,count_digit[i]);
+    //    }
     int i;
-    for (i=0; i<10;i++){
-        printf("%d--> %d\n",i,count_digit[i]);
+    for (i=0;i<10;i++){
+        if (count_digit[i]>1)
+        return 0;
     }
+    return 1;
 }
 int main()
 {
-    int n;
-    printf("Enter a year:");
-    scanf("%d",n);
-    is_leap_year(n);
-    digit_district(n);
-     return 0;
+    int year;
+    printf("enter a year: ");
+    scanf("%d",&year);
+    if (is_leap_year(year)==1 && digit_district(year)==1)
+    {
+        printf("this is year is leapyear and digit distrinct year..");
+    }
+    else
+    printf("this is year is NOT digit distrinct year..");
+
+    return 0;
 }
