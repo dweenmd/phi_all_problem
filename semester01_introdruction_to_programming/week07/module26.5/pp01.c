2df25b2841a39329cdc68279fa1c3bf2b194121f
dweenmd <dween.md01@gmail.com>
2023-02-13 20:09:55 +0600

    pp01-26.5

---
+++ semester01_introdruction_to_programming/week07/module26.5/pp01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    float a,b,avg;
+    scanf("%f %f",&a,&b);
+    float *n,*m;
+    n=&a;
+    m=&b;
+    avg=(*n+*m)/2;
+    printf("%.2f",avg);
+
+    return 0;
+}
