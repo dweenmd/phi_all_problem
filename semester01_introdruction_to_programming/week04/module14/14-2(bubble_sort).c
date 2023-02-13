5a11a998644db793d1d56d057445e6be0b8540fb
dweenmd <dween.md01@gmail.com>
2023-02-14 03:05:29 +0600

    update

--- semester01_introdruction_to_programming/week04/module14/14-2(bubble_sort).c
+++ semester01_introdruction_to_programming/week04/module14/14-2(bubble_sort).c
@@ -1,39 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int arr[]={12,7,9,3,15,2,6,5};
-    int size=sizeof (arr)/sizeof(arr[0]);
-    int i;
+    int arr[] = {12, 7, 9, 3, 15, 2, 6, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    // int size = sizeof(arr)/sizeof(int);
+
     // print all array
-    for (int i=0; i<size ; i++)
+    for (int i = 0; i < size; i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d ", arr[i]);
     }
     printf("\n");
 
- for (int j=0;j<size-1; j++)
- {
-
-    for (int i=0; i<size-1; i++)
+    for (int j = 0; j < size - 1; j++)
     {
-        //compare korbo arr[i] and arr[i+1] er moddhe
 
-        if (arr[i]>arr[i+1])
+        for (int i = 0; i < size - 1; i++)
         {
-            int temp=arr[i];  //swaping
-            arr[i]=arr[i+1];
-            arr[i+1]=temp;
-        }
+            // compare korbo arr[i] and arr[i+1] er moddhe
 
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i]; // swaping
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
     }
 
- }
-
- for(int i=0; i<size; i++){
-     printf("%d ", arr[i]);
-     }
-
-
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
--- semester01_introdruction_to_programming/week04/module14/Petya_and_Strings-112A.c
+++ semester01_introdruction_to_programming/week04/module14/Petya_and_Strings-112A.c
@@ -1,25 +1,28 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main()
 {
-    char a [103],b[103];
-    fgets(a,sizeof(a),stdin);
-    fgets(b,sizeof(b),stdin);
-    int len =strlen(a)-1;
-    for(int i=0; i<len; i++){
-       if (a[i]>='A'&& a[i]<='Z'){
-            a[i]+=32;
+    char a[103], b[103];
+    fgets(a, sizeof(a), stdin);
+    fgets(b, sizeof(b), stdin);
+    int len = strlen(a) - 1;
+    for (int i = 0; i < len; i++)
+    {
+        if (a[i] >= 'A' && a[i] <= 'Z')
+        {
+            a[i] += 32;
         }
-         if (b[i]>='A'&& b[i]<='Z'){
-            b[i]+=32;
+        if (b[i] >= 'A' && b[i] <= 'Z')
+        {
+            b[i] += 32;
         }
-        if(a[i]>b[i])
+        if (a[i] > b[i])
         {
             printf("1");
             return 0;
         }
 
-        else if (a[i]<b[i])
+        else if (a[i] < b[i])
         {
             printf("-1");
             return 0;
@@ -28,6 +31,6 @@ int main()
         printf("%c",b[i]);*/
     }
 
-        printf("0\n");
+    printf("0\n");
     return 0;
 }
