aafc51c62ee37cd980929d096d7c35de5af407d7
dweenmd <dween.md01@gmail.com>
2023-02-22 21:39:13 +0600

    ans 07

--- semester 1 Exam and assignment/labAssignment2_module27/ans07Swap.c
+++ semester 1 Exam and assignment/labAssignment2_module27/ans07Swap.c
@@ -1,7 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+void swap(int arr[], int x, int y)
+{
+    int *X, *Y;
+    X = &arr[x];
+    Y = &arr[y];
+
+    int temp = *X;
+    *X = *Y;
+    *Y = temp;
+}
 int main()
 {
-
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d ", &arr[i]);
+    }
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        swap(arr, x, y);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
