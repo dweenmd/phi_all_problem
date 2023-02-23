4b0d6783532210de1b940088387c77a697fdc606
dweenmd <dween.md01@gmail.com>
2023-02-23 23:50:21 +0600

    update

---
+++ semester 1 Exam and assignment/labAssignment2_module27/ans10BeautyfulArrayLast.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+int isNiceArray(int n, int *arr)
+{
+    int r=ceil(n/2);
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x = arr[i];
+
+        while (x > 0)
+        {
+            if (x % 10 == 7)
+            {
+                count++;
+                break;
+            }
+            x /= 10;
+        }
+    }
+
+    if (count >= r)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int r = isNiceArray(n, arr);
+
+    if (r == 1)
+    {
+        printf("Beautiful");
+    }
+
+    else
+    {
+        printf("Ugly");
+    }
+
+}
