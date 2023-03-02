444ccd31ea5b27328aae123d0bb7a374eff64cd2
dweenmd <dween.md01@gmail.com>
2023-03-02 15:06:10 +0600

    up

--- semester01_introdruction_to_programming/week08/module30,lab/midpoint_findpb2.c
+++ semester01_introdruction_to_programming/week08/module30,lab/midpoint_findpb2.c
@@ -1,19 +1,35 @@
-#include<stdio.h>
-struct point {
-    int x;
-    int y;
+#include <stdio.h>
+struct point
+{
+    float x;
+    float y;
 };
-int main(){
-    struct point p1={1,1};
-    struct point p2={4,5};
+struct point mid(struct point m, struct point n)
+{
+    struct point ans ;
+    ans.x=(m.x+n.x)/2;
+    ans.y=(m.y+n.y)/2;
+    return ans;
+};
+int main()
+{
+    struct point p1 = {1, 1};
+    struct point p2 = {4, 5};
     struct point p3;
 
-     /* 
-     p3=p1+p2;  //evabe likhle kono output show korbe na
-     */
+    /*
+    p3=p1+p2;  //evabe likhle kono output show korbe na
+    */
+    // when p3 = p2 tokhon resust show korbe
+    p3 = p2;
+    printf(" when p3 = p2: %.1f, %.1f", p3.x, p3.y);
+    // create a function that return point or struct that is(struct point mid(struct point m, struct point))
 
-    printf("%d, %d",p3.x,p3.y);
+    p3 = mid(p1,p2);
+    
+    //now print the mid value of p1 & p2 point
 
+    printf("\nThe mid point is %0.2f, %0.2f", p3.x,p3.y);
 
     return 0;
 }
