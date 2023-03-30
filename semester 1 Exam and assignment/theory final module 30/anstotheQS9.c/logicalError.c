816a6bc676d4142becde64a5cecbcc065f791374
Dween MOhammad <dween.md01@gmail.com>
2023-03-30 14:40:50 +0600

    error edit

--- semester 1 Exam and assignment/theory final module 30/anstotheQS9.c/logicalError.c
+++ semester 1 Exam and assignment/theory final module 30/anstotheQS9.c/logicalError.c
@@ -3,8 +3,7 @@ int main()
 {
     int sum = 0; // variable initialization
     int k = 1;
-    for (int i = 1; i <= 10; i++)
-        ; // logical error, as we put the semicolon after loop
+    for (int i = 1; i <= 10; i++); // logical error, as we put the semicolon after loop
     {
         sum = sum + k;
         k++;
