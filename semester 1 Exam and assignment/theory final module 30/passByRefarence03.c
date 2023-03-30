83823caa430b4ff7e230f0fc0ccca9bffe92b3b9
Dween MOhammad <dween.md01@gmail.com>
2023-03-30 13:45:58 +0600

    m

---
+++ semester 1 Exam and assignment/theory final module 30/passByRefarence03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void swap(int *a, int *b);
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    swap(&a, &b);
+    printf("After Swap: %d %d", a, b);
+    return 0;
+}
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
