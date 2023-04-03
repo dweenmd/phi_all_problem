6f86fd1ef3877c3c7bb93751e754a247a9d0d80c
Dween MOhammad <dween.md01@gmail.com>
2023-04-03 16:49:16 +0600

    32-2

--- semester01_introdruction_to_programming/week09(file and recursion)/module 32/printNto1usingRecursion.c
+++ semester01_introdruction_to_programming/week09(file and recursion)/module 32/printNto1usingRecursion.c
@@ -1,1 +1,14 @@
-#include<s
+#include<stdio.h>
+void solve(int n,int i){
+     if (n<i) return;
+     printf("\n%d",n);
+     solve(n-1,i);
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    solve(n,1);
+    return 0;
+
+}
