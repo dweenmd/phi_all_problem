5a1f9de8d724b8b5ab2255f6b1ff90c03c872fe1
Dween MOhammad <dween.md01@gmail.com>
2023-04-03 21:41:38 +0600

    m

--- semester01_introdruction_to_programming/week09(file and recursion)/module 32/RecursionwithArray2.c
+++ semester01_introdruction_to_programming/week09(file and recursion)/module 32/RecursionwithArray2.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+int solve(int arr[], int n)
+{
+    if (n == 0)
+        return 0;
+    int s = solve(arr + 1, n - 1);
+    return s + (*arr);
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
 
+            scanf("%d", &arr[i]);
+    }
+    int s=solve(arr,n);
+    printf("%d",s);
+
+    return 0;
+}
