65dcd8a56c7bef02ef0080b0cf48c38a200cd3db
Dween MOhammad <dween.md01@gmail.com>
2023-04-05 17:57:29 +0600

    update tik tok toe

--- semester01_introdruction_to_programming/week09(file and recursion)/module 34 tik tok toe/tikTokToe.c
+++ semester01_introdruction_to_programming/week09(file and recursion)/module 34 tik tok toe/tikTokToe.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void printcell(int a[][4], int n);
-int isWin(int a[][4], int n);
+void printcell(int dween[][4], int n);
+int isWin(int dween[][4], int n);
 
 int main()
 {
     int n = 3;
-    int a[4][4];
+    int dween[4][4];
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            a[i][j] = -1;
+            dween[i][j] = -1;
         }
     }
     // player input
@@ -20,19 +20,19 @@ int main()
     bool player2 = false;
     while (true)
     {
-        printcell(a, n);
+        printcell(dween, n);
         if (player1 == true)
         {
             int r, c;
         flag:
             printf("Player 1 Turn(X), Enter row  and coloumn:");
             scanf("%d %d", &r, &c);
-            if (a[r][c] != -1)
+            if (dween[r][c] != -1)
             {
                 printf("\nInvalid Cell, again try\n");
                 goto flag;
             }
-            a[r][c] = 1;
+            dween[r][c] = 1;
             player1 = false;
             player2 = true;
         }
@@ -42,26 +42,26 @@ int main()
         flag2:
             printf("Player 2 Turn(0), Enter row  and coloumn:");
             scanf("%d %d", &r, &c);
-            if (a[r][c] != -1)
+            if (dween[r][c] != -1)
             {
                 printf("\nInvalid Cell, again try\n");
                 goto flag2;
             }
-            a[r][c] = 2;
+            dween[r][c] = 2;
             player2 = false;
             player1 = true;
         }
         // who is win? player 1 or player 2
-        if (isWin(a, n) == 1)
+        if (isWin(dween, n) == 1)
         {
-            printcell(a, n);
+            printcell(dween, n);
             printf("PLAYER 1 WON THE GAME!!!...\n");
 
             break;
         }
-        else if (isWin(a, n) == 2)
+        else if (isWin(dween, n) == 2)
         {
-            printcell(a, n);
+            printcell(dween, n);
             printf("PLAYER 2 WON THE GAME!!!...\n");
 
             break;
@@ -71,17 +71,17 @@ int main()
     return 0;
 }
 // function of printing array
-void printcell(int a[][4], int n)
+void printcell(int dween[][4], int n)
 {
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            if (a[i][j] == -1)
+            if (dween[i][j] == -1)
                 printf(" ");
-            if (a[i][j] == 1)
+            if (dween[i][j] == 1)
                 printf("X");
-            if (a[i][j] == 2)
+            if (dween[i][j] == 2)
                 printf("0");
             if (j < n)
                 printf("\t|\t");
@@ -93,33 +93,33 @@ void printcell(int a[][4], int n)
     }
 }
 // is win cheaking function
-int isWin(int a[][4], int n)
+int isWin(int dween[][4], int n)
 {
     // cheaking horizontal position
     for (int i = 1; i <= n; i++) // i = row
     {
-        if (a[i][1] == a[i][2] && a[i][2] == a[i][3] && a[i][1] != -1)
+        if (dween[i][1] == dween[i][2] && dween[i][2] == dween[i][3] && dween[i][1] != -1)
         {
-            return a[i][1];
+            return dween[i][1];
         }
     }
 
     // cheaking verticale position
     for (int j = 1; j <= n; j++) // j is coloumn
     {
-        if (a[1][j] == a[2][j] && a[2][j] == a[3][j] && a[1][j] != -1)
+        if (dween[1][j] == dween[2][j] && dween[2][j] == dween[3][j] && dween[1][j] != -1)
         {
-            return a[1][j];
+            return dween[1][j];
         }
     }
     // cheaking diagonal position
-    if (a[1][1] == a[2][2] && a[2][2] == a[3][3] && a[1][1] != -1)
+    if (dween[1][1] == dween[2][2] && dween[2][2] == dween[3][3] && dween[1][1] != -1)
     {
-        return a[1][1];
+        return dween[1][1];
     }
-    if (a[1][3] == a[2][2] && a[2][2] == a[3][1] && a[3][1] != -1)
+    if (dween[1][3] == dween[2][2] && dween[2][2] == dween[3][1] && dween[3][1] != -1)
     {
-        return a[1][3];
+        return dween[1][3];
     }
 
     return -1;
